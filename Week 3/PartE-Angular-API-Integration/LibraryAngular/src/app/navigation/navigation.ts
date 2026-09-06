69586c206ebd6ab61b14d80192f0ef3301ca3537
Eman Fatima <emanfatima6425@gmail.com>
2026-09-06 20:57:42 +0500

    feat: connect Angular BookService to live API with loading and error states

---
+++ Week 3/PartE-Angular-API-Integration/LibraryAngular/src/app/navigation/navigation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation',
+  imports: [RouterLink],
+  templateUrl: './navigation.html',
+  styleUrl: './navigation.css'
+})
+export class Navigation {
+}
