f71ec8d13cda32efb956d99be78eb199826d4d76
Eman Fatima <emanfatima6425@gmail.com>
2026-09-08 13:59:33 +0500

    feat: add JWT authentication with register, login, and role-based authorization

---
+++ Week 4/PartA-JWT-Authentication/LibraryApi/DTOs/RegisterDto.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryApi.DTOs;
+
+public class RegisterDto
+{
+    [Required]
+    public string Username { get; set; }
+        = string.Empty;
+
+    [Required]
+    [MinLength(6)]
+    public string Password { get; set; }
+        = string.Empty;
+}
