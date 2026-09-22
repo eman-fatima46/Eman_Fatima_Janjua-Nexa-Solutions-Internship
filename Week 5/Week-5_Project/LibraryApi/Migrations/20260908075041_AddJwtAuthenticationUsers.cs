8e33a0b84a12d16b378751a2b30dc24a71154389
Eman Fatima <emanfatima6425@gmail.com>
2026-09-22 20:22:50 +0500

    chore: initialize Week 5 project from Week 4 application

---
+++ Week 5/Week-5_Project/LibraryApi/Migrations/20260908075041_AddJwtAuthenticationUsers.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddJwtAuthenticationUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Users",
+                newName: "Username");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Users",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Users",
+                newName: "Email");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Users",
+                newName: "UserId");
+        }
+    }
+}
---
+++ Week 5/Week-5_Project/LibraryApi/Models/BookCategory.cs
@@ -0,0 +1,12 @@
+namespace LibraryApi.Models;
+
+public class BookCategory
+{
+    public int BookId { get; set; }
+
+    public Book? Book { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public Category? Category { get; set; }
+}
